b28dbc9103a6b9378a6a4b61d7de6d63a60b88aa
garrett laroy johnson <garrett.laroy.johnson@gmail.com>
2023-04-14 10:19:05 -0500

    created object

--- p5.korgnano.js
+++ p5.korgnano.js
@@ -1,9 +1,56 @@
 // thanks to dbarrett: https://editor.p5js.org/dbarrett/sketches/HJhBG-LI7
 
+let freshMIDI = [];
+
 class KorgNano {
   constructor(type) {
     this.device = type;
     this.init(this);
+    this.faders = [0, 0, 0, 0, 0, 0, 0, 0];
+    this.dials = [0, 0, 0, 0, 0, 0, 0, 0];
+    this.m = [0, 0, 0, 0, 0, 0, 0, 0];
+    this.s = [0, 0, 0, 0, 0, 0, 0, 0];
+    this.r = [0, 0, 0, 0, 0, 0, 0, 0];
+    this.track = [0, 0];
+    this.cycle = 0;
+    this.marker = [0, 0, 0];
+    this.play = 0;
+    this.record = 0;
+    this.stop = 0;
+    this.ff = 0;
+    this.rw = 0;
+  }
+  update() {
+    let n = freshMIDI[0];
+    let v = freshMIDI[1];
+    if (n < 8) {
+      this.faders[n] = v;
+    } else if (n >= 16 && n <= 23) {
+      this.dials[n - 16] = v;
+    } else if (n >= 32 && n <= 39) {
+      this.s[n - 32] = v;
+    } else if (n == 41) {
+      this.play = v;
+    } else if (n == 42) {
+      this.stop = v;
+    } else if (n == 43) {
+      this.rw = v;
+    } else if (n == 44) {
+      this.ff = v;
+    } else if (n == 45) {
+      this.stop = v;
+    } else if (n == 46) {
+      this.cycle = v;
+    } else if (n >= 48 && n <= 55) {
+      this.m[n - 48] = v;
+    } else if (n >= 58 && n <= 59) {
+      this.track[n - 58] = v;
+    } else if (n >= 60 && n <= 62) {
+      this.marker[n - 60] = v;
+    } else if (n >= 64 && n <= 71) {
+      this.r[n - 64] = v;
+    }
+    console.log(this);
   }
   init(ref) {
     WebMidi.enable(function (err) {
@@ -22,19 +69,21 @@ class KorgNano {
         console.log(i + ": " + WebMidi.inputs[i].name);
       }
 
-      console.log(ref.device);
-      //Choose an input port
+      // take JS object argument to look for device name
       let index = WebMidi.inputs.findIndex((x) => x.name === ref.device);
       let inputSoftware = WebMidi.inputs[index];
-      //The 0 value is the first value in the array
-      //Meaning that we are going to use the first MIDI input we see
-      //This can be changed to a different number,
-      //or given a string to select a specific port
+
+      if (inputSoftware) {
+        console.log("connected to " + ref.device);
+      } else {
+        console.log("error connecting to MIDI device");
+      }
 
       // Listen to control change message on all channels
       inputSoftware.addListener("controlchange", "all", function (e) {
-        console.log("Received 'controlchange' message.", e.value);
-        console.log(e.controller.number);
+        displayText = e.data[1] + " " + e.data[2];
+        freshMIDI = [e.data[1], e.data[2]];
+        ref.update();
       });
 
       //
--- sketch.js
+++ sketch.js
@@ -1,18 +1,33 @@
-//background color variable
-var bgColor;
-
 //color of a square
 var squareColor;
 
 //text to be displayed
 var displayText;
 
-//sound to be played
-var soundFX;
-
+// declare a varible that is going to hold our KorgNano object.
 let b;
 
+let c1; //will hold color1
+let c2; // will hold color2
+
+let c3; //will hold color3
+let c4; // will hold color4
+
+let x;
+let y;
+
+let r = 30;
+
+let angle = 0.0;
+
+let speed = 0.01;
+
+let sqr;
+
 function setup() {
+  b = new KorgNano("nanoKONTROL2");
+  sqr = new GradientSquare();
+
   //400 by 400 pixel canvas
   createCanvas(400, 400);
 
@@ -25,23 +40,50 @@ function setup() {
   //starting text
   displayText = "Nothing received";
 
-  ////
-  //Setting up MIDI
-  ////
-  b = new KorgNano("nanoKONTROL2");
+  // assign colors to p5 color objects
+  c1 = color("#40c9ff");
+  console.log(c1);
+  c2 = color("#e81cff");
+  c3 = color("#bf0fff");
+  c4 = color("#cbff49");
+
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
-  //Draw background with background color variable
-  //Will change every time there is a note on
-  background(bgColor);
+  // access faders by using dot notation, then the array name faders, then their position in the array.
+
+  let xfade1 = b.faders[0] / 127; // remember MIDI values always default to 0-127, so we need to scale. lerpColor() will expect a floating point value 0-1.
+  let xfade2 = b.faders[1] / 127;
+  // these scaled variables drive interpolation between one color of the gradient.
+  let c5 = lerpColor(c1, c2, xfade1);
+  let c6 = lerpColor(c3, c4, xfade2);
+
+  // draw gradient;
+  for (let x = 0; x < width; x++) {
+    let interp = x / width;
+    let c7 = lerpColor(c5, c6, interp);
+    stroke(c7);
+    line(x, 0, x, height);
+  }
 
   //Drawing a rectangle, with no outline,
   //Middle of the screen (width and height divided by two)
   //Changes
   fill(squareColor);
   noStroke();
-  rect(100, 100, width / 2, height / 2);
+
+  rect(x, y, width / 2, height / 2);
+
+  x = sin(angle) * r;
+  y = cos(angle) * r;
+
+  angle += speed;
+
+  // update r and speed according to midi fader controls
+  r = faders[0];
+  speed = faders[1] * 0.001;
 
   //Displaying text
   //Little bit under the box above
@@ -50,4 +92,25 @@ function draw() {
   textAlign(CENTER);
   textSize(20);
   text(displayText, width / 2, 350);
+}
+
+class GradientSquare {
+  constructor() {
+    this.x = x; // left most line
+    this.y = y; // topmost line
+    this.w = w; // width of rect
+    this.c1; //
+    this.c2;
+  }
+  update(c1, c2) {
+    this.c1 = c1;
+    this.c2 = c2;
+  }
+  display() {
+    for (let x = 0; x < this.w; x++) {
+      let col = lerpColor(this.c2, this.c1, x);
+      stroke(col);
+      line(this.x + x, this.y, this.x + x, this.y + this.w);
+    }
+  }
 }
